f4beb240b4dae2b55d8d05f334bc6cea6069c598
Sourabh Majumdar <majumdars844@gmail.com>
2021-07-19 21:32:03 +0530

    Created Expense basic UI and basic form

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ExpenseItem from "./ExpenseItem";
+import './Expenses.css';
+import Card from "../UI/Card";
+
+function Expenses(props) {
+  return (
+    <Card className="expenses">
+      <ExpenseItem
+        title={props.expenses[0].title}
+        amount={props.expenses[0].amount}
+        date={props.expenses[0].date}
+      />
+      <ExpenseItem
+        title={props.expenses[1].title}
+        amount={props.expenses[1].amount}
+        date={props.expenses[1].date}
+      />
+      <ExpenseItem
+        title={props.expenses[2].title}
+        amount={props.expenses[2].amount}
+        date={props.expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.expenses[3].title}
+        amount={props.expenses[3].amount}
+        date={props.expenses[3].date}
+      />
+    </Card>
+  );
+}
+
+export default Expenses;
