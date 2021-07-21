9cb6b18a1c60be00c424790e3ea5b60a65668aa5
Sourabh Majumdar <majumdars844@gmail.com>
2021-07-21 20:39:50 +0530

    Rendering list of data and using stateful lists

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -15,26 +15,15 @@ function Expenses(props) {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      <ExpenseItem
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-        date={props.expenses[0].date}
-      />
-      <ExpenseItem
-        title={props.expenses[1].title}
-        amount={props.expenses[1].amount}
-        date={props.expenses[1].date}
-      />
-      <ExpenseItem
-        title={props.expenses[2].title}
-        amount={props.expenses[2].amount}
-        date={props.expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.expenses[3].title}
-        amount={props.expenses[3].amount}
-        date={props.expenses[3].date}
-      />
+      {
+        props.items.map((expense) => 
+        <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+        />
+        )
+      }
     </Card>
   );
 }
